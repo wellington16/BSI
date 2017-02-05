bb54edacfc9f56a510682f7026ce3173d3bb3d14
Wellington <wjfilmagens@hotmail.com>
2017-02-05 15:48:49 -0200

    atualizando o git com as coisas novas

---
+++ Períodos Anteriores/adminantração financeiras/indeces de LiqEndivRent.py
@@ -0,0 +1,9 @@
+ac=int(input("AC: "))
+pc=int(input("PC: "))
+rlp=int(input("RLP: "))
+elp=int(input("ELP: "))
+e = int(input("Estoque: "))
+d = int(input("Disponibilidade: "))
+pnc = elp
+
+pl = int(input("Patrimonio L.: "))
